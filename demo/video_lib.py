0c2420a53287d74d7ecb088fb68904e94ffbde95
Connor Lee <conn.dlee@gmail.com>
2024-11-26 17:29:20 -0500

    feat: working live video demo

--- demo/video_lib.py
+++ demo/video_lib.py
@@ -3,6 +3,8 @@
 # HELPER FUNCTIONS FOR VIDEO DEMO
 # Connor Lee
 import cv2 as cv
+import torch.nn as nn
+import torch.nn.functional as F
 
 def open_video(n=0):
     capture = cv.VideoCapture(n)
@@ -11,5 +13,16 @@ def open_video(n=0):
         return None
     return capture
 
+class FCCNet(nn.Module):
+    def __init__(self):
+        super(FCCNet, self).__init__()
+        self.name = "FCClassifier"
+        self.fc1 = nn.Linear(256 * 6 * 6, 64)
+        self.fc2 = nn.Linear(64, 9)
 
+    def forward(self, x):
+        x = x.reshape(-1, 256 * 6 * 6)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
